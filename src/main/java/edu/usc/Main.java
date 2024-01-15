854f0eb4e172c889b002d56ad4bcbf93cc23388b
Robert <rwinn@usc.edu>
2024-01-15 14:00:05 -0800

    setup

--- src/main/java/edu/usc/Main.java
+++ src/main/java/edu/usc/Main.java
@@ -1,7 +1,12 @@
 package edu.usc;
 
+import edu.usc.Utilities.LoadConfig;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Properties Configs = LoadConfig.main();
+        System.out.println(Configs.getProperty("subject"));
     }
 }
---
+++ src/main/java/edu/usc/Utilities/LoadConfig.java
@@ -0,0 +1,23 @@
+package edu.usc.Utilities;
+
+import java.io.*;
+import java.util.Properties;
+
+public class LoadConfig {
+
+    public static Properties main() {
+        try {
+            String configFilePath = "config.txt";
+            FileInputStream propsInput = new FileInputStream(configFilePath);
+            Properties prop = new Properties();
+            prop.load(propsInput);
+            return prop;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
