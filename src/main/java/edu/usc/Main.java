bbd9d8cc5943b564a535ea81a81c42b20545f057
rwinn1103 <rwinn@usc.edu>
2024-01-19 09:33:13 -0800

    Successful setup of BG on a single webpage

--- src/main/java/edu/usc/Main.java
+++ src/main/java/edu/usc/Main.java
@@ -1,7 +1,11 @@
 package edu.usc;
 
+import edu.usc.ChromeTest;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ChromeTest TestingChrome = new ChromeTest();
+        TestingChrome.main();
     }
 }
